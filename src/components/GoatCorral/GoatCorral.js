40f1b902ec14c72ad6f4bc314611dcea2ed25257
Silvestre Luna <slvtrluna@gmail.com>
2019-06-17 20:04:07 -0500

    Priting each goat's name to the DOM

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -5,10 +5,17 @@ import './GoatCorral.scss';
 
 class GoatCorral extends React.Component {
   render() {
+    const { goats } = this.props;
+    const makeGoats = goats.map(goat => (
+      <h2>{goat.name}</h2>
+      // <Goat goat={goat} />
+    ));
+
     return (
       <div>
-        <button>GOATCORRAL</button>
-        {console.error(this.props.goats)}
+        {/* <button>GOATCORRAL</button>
+        {console.error(this.props.goats)} */}
+        { makeGoats }
       </div>
     );
   }
